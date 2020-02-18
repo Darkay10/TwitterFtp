d32e41be61dbbaad73010d3b1f5dc2684feb83c4
Darkay10 <59738162+Darkay10@users.noreply.github.com>
2020-02-18 17:43:30 +0100

    Grande
    Bien

---
+++ app/src/main/java/es/fempa/pmdm/ftptwitter/MainActivity.java
@@ -0,0 +1,41 @@
+package es.fempa.pmdm.ftptwitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText eUsu;
+    EditText ePunt;
+    Button bEnv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        eUsu = findViewById(R.id.eUsuario);
+        ePunt = findViewById(R.id.ePuntuacion);
+        bEnv = findViewById(R.id.bEnviar);
+    }
+
+    public void enviar (View v) {
+        String u = eUsu.getText().toString();
+        String p = ePunt.getText().toString();
+
+        if (p.equals("") || u.equals("")) {
+            toastRespuesta("Has dejado algún campo vacío");
+        }
+        else {
+            toastRespuesta("Ahora hemos de hacer la función ftp y de twitter");
+        }
+    }
+
+    public void toastRespuesta (String texto) {
+        Toast res = Toast.makeText(getApplicationContext(), texto, Toast.LENGTH_SHORT);
+        res.show();
+    }
+}
