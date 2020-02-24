1b49064cbd5116253630fc10378f806d5349794f
divi817 <58705833+divi817@users.noreply.github.com>
2020-02-24 20:02:58 +0100

    Update TwitPuntos.java

--- app/src/main/java/es/fempa/pmdm/ftptwitter/TwitPuntos.java
+++ app/src/main/java/es/fempa/pmdm/ftptwitter/TwitPuntos.java
@@ -1,18 +1,18 @@
 package es.fempa.pmdm.ftptwitter;
 
-import twitter4j.Twitter;
+import twitter4j.conf.*;
 
 public class TwitPuntos {
     private Twitter twitter;
     public TwitPuntos() {
-       /* ConfigurationBuilder cb = new ConfigurationBuilder();
+       ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("*********************")
-                .setOAuthConsumerSecret("******************************************")
-                .setOAuthAccessToken("**************************************************")
-                .setOAuthAccessTokenSecret("******************************************");
+                .setOAuthConsumerKey("tFIzxPJ3CqfcHQLDDaK87Y1LP")
+                .setOAuthConsumerSecret("uqbMK3KQf4i4PULkgDK5HLfGpRpKDNVYwNKsDXi65AEnWT1KaS")
+                .setOAuthAccessToken("1229778457938321408-MObDFO5wO1zaFRzmLBGf7vkqtVISlG")
+                .setOAuthAccessTokenSecret("myJgWgwTmjNolkE4JFUIfgP7c4uqF6bTD3kjSt0hxoNWS");
         TwitterFactory tf = new TwitterFactory(cb.build());
-        Twitter twitter = tf.getInstance();*/
+        Twitter twitter = tf.getInstance();
     }
 
 }
